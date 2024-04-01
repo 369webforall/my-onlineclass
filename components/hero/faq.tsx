fe9bfc52b96ff39803b48afbd425f2f61e836378
369webforall <369webforall@gmail.com>
2024-04-01 18:47:24 +0545

    update

--- components/hero/faq.tsx
+++ components/hero/faq.tsx
@@ -17,13 +17,14 @@ const Faq = () => {
     <AccordionContent>
       Yes. It adheres to the WAI-ARIA design pattern.
     </AccordionContent>
+    </AccordionItem>
     <AccordionItem value="item-2">
     <AccordionTrigger>Is it accessible?</AccordionTrigger>
     <AccordionContent>
       Yes. It adheres to the WAI-ARIA design pattern.
     </AccordionContent>
     </AccordionItem>
-    <AccordionItem value="item-3"></AccordionItem>
+    <AccordionItem value="item-3">
     <AccordionTrigger>Is it accessible?</AccordionTrigger>
     <AccordionContent>
       Yes. It adheres to the WAI-ARIA design pattern.
